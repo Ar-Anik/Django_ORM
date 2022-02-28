9b551fa779fdcfee830653f0bc4ab2c5b00dcba0
Ar-Anik <67730427+Ar-Anik@users.noreply.github.com>
2022-02-28 13:11:17 +0600

    update

--- Django_Many_To_Many_Relationship/core/many/views.py
+++ Django_Many_To_Many_Relationship/core/many/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from .models import Student 
+from .models import Student
 
 # Create your views here.
 
 def student_list(request):
-    data = Student.objects.all() 
+    data = Student.objects.all()
 
     context = {
         'data' : data,
     }
 
     return render(request, 'studentlist.html', context)
-
